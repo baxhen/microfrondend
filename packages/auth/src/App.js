55dc963bafe858c35577220216c746a1426bff43
Leonardo Rebouças Batista <leo292629@gmail.com>
2021-04-03 19:29:47 -0300

    first commit

---
+++ packages/auth/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'au',
+});
+
+export default ({ history, onSignIn }) => {
+  return (
+    <div>
+      <Router history={history}>
+        <StylesProvider generateClassName={generateClassName}>
+          <Switch>
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
+          </Switch>
+        </StylesProvider>
+      </Router>
+    </div>
+  );
+};
